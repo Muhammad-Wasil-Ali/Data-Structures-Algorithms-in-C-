8565599d2e62552cc01380fca43080e071f38a25
Muhammad Wasil Ali <wasilali4999@gmail.com>
2026-01-07 18:58:23 +0500

    Updated binary_number_system/1.cpp

--- binary_number_system/1.cpp
+++ binary_number_system/1.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+
+// this fucntion is able to convert the decimal number into binary number 
+
+// e-g  12  to  1100
 int decimalToBinary(int decimalNumber){
     int ans=0;
     int pow=1;
@@ -14,7 +18,28 @@ int decimalToBinary(int decimalNumber){
 
     return ans;
 }
+
+// this fucntion is able to convert the binary number into decimal number
+
+// e-g  1100  to  12
+
+
+int binaryToDecimal(int binaryNumber){
+    int ans=0;
+    int pow=1;
+    while(binaryNumber>0){
+        int remainder=binaryNumber%10;
+        binaryNumber=binaryNumber/10;
+
+        ans+=remainder*pow;
+        pow*=2;
+    }
+
+    return ans;
+}
 int main(){
-cout<<decimalToBinary(15);
+cout<<"Decimal to binary "<<decimalToBinary(15)<<endl;
+cout<<"Binary to Decimal "<<binaryToDecimal(101)<<endl;
+
     return 0;
 }
