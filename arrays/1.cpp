647bf6c5c6007e9c75ebad6b06fcad429447784b
Muhammad Wasil Ali <wasilali4999@gmail.com>
2026-01-08 20:51:33 +0500

    Updating arrays solved find min and max in array

--- arrays/1.cpp
+++ arrays/1.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int findMax(int arr[],int size){
+    int largest=arr[0];
+    for(int i=0;i<size;i++){
+largest=max(arr[i],largest);
+}
+return largest;
+}
+int findMin(int arr[],int size){
+    int smallest=arr[0];
+    for(int i=0;i<size;i++){
+smallest=min(arr[i],smallest);
+}
+return smallest;
+}
 int main(){
     
 
     int size=6;
-    int arr[6]={1,2,3,4,5,10};
-
-    for (int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
-    }
+    int arr[6]={1,2,3,4,5,-10};
+cout<<findMin(arr,size);
+cout<<endl;
+cout<<findMax(arr,size);
+   
     return 0;
 }
--- patterns/2.cpp
+++ patterns/2.cpp
@@ -3,11 +3,26 @@ using namespace std;
 int main(){
 
     int n=6;
+
+    //  patterns with similar logic of characters and numbers
+
+    // pattern---01
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cout<<j+1<<" ";
         }
         cout<<endl;
     }
+
+// pattern---02
+    for(int i=0;i<n;i++){
+        char a='A';
+        for(int j=0;j<n;j++){
+            cout<<a<<" ";
+            a+=1;
+        }
+        cout<<endl;
+    }
+  
     return 0;
 }
