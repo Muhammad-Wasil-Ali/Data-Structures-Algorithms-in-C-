2a80c48d05fe86478745e27b7d247c5f7831c59e
Muhammad Wasil Ali <wasilali4999@gmail.com>
2026-01-08 20:29:21 +0500

    Understood arrays

---
+++ arrays/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+
+    int size=6;
+    int arr[6]={1,2,3,4,5,10};
+
+    for (int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
