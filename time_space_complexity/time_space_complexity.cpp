f5fa754bd04b31ed171a631ff2464a022796c0ac
Muhammad Wasil Ali <wasilali4999@gmail.com>
2026-02-09 20:57:04 +0500

    Undersatnd time and space complexity

---
+++ time_space_complexity/time_space_complexity.cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    // Time complexity:  measures how much time an algorithm takes to run as the input size grows.
+// It helps us compare algorithms based on speed and efficiency.
+
+// Space complexity: measures how much memory an algorithm uses as the input size grows.
+// It helps us understand how memory-efficient an algorithm is.
+    return 0;
+}
