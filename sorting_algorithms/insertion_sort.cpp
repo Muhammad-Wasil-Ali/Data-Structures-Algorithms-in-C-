168dfd86dcaf0f8e9f8f801058f8412db72d6892
Muhammad Wasil Ali <wasilali4999@gmail.com>
2026-02-17 21:28:19 +0500

    insertion sort solved

---
+++ sorting_algorithms/insertion_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+
+int insertionSort(int arr[],int n){
+    for(int i=1;i<n;i++){
+        int curr=arr[i];
+        int prev=i-1;
+
+        while(prev>=0&&arr[prev]>curr){
+            arr[prev+1]=arr[prev];
+            prev--;
+        }
+
+        arr[prev+1]=curr;
+    }
+}
+
+void printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+}
+int main(){
+
+    int arr[]={5,6,3,4,9,1};
+    int n=6;
+
+    insertionSort(arr,n);
+
+    printArray(arr,n);
+    return 0;
+}
